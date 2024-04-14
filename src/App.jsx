86c25d76ca1d32fd7f70e20b1b5d2e691d71fa20
aiXpertLab <aiXpertLab@gmail.com>
2024-04-14 09:00:22 -0400

    v1_workable

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+import Signup from './Signup'
+import Login from './Login'
+import Home from './Home'
+
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/register' element={<Signup/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/home' element={<Home />}/>
+        <Route path='/' element={<Home />}/>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
